442d74e395e1bf89a5331f15e95577ad7ecbfdad
anon <anon@anon.no>
2019-02-01 10:05:14 -0500

    Do not remove other sites in nginx when deploying

--- scripts/start_primary_production.sh
+++ scripts/start_primary_production.sh
@@ -7,7 +7,7 @@ job_primary.py > job_primary.log 2>&1 & echo $! > job_primary.pid
 
 # restart nginx for production static server
 # assumes we're in modelrunner dir
-sudo rm -f /etc/nginx/sites-enabled/*
+#sudo rm -f /etc/nginx/sites-enabled/*
 sudo cp devops/primary.nginx /etc/nginx/sites-available/primary
 sudo ln -s /etc/nginx/sites-available/primary /etc/nginx/sites-enabled/
 sudo service nginx restart 
