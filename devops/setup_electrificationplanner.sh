6718fda4aba6d173e4379a9b2fdd271c3ec605c8
Your Name <you@example.com>
2019-02-04 10:26:09 -0500

    Move package list locally

--- devops/setup_electrificationplanner.sh
+++ devops/setup_electrificationplanner.sh
@@ -10,12 +10,8 @@ echo "Setup electrificationplanner env"
 # electrificationplanner requires full source to run
 rm -rf $HOME/infrastructure-planning
 git clone https://github.com/piensa/infrastructure-planning
-pushd infrastructure-planning
-# get to commit with pinned versions
-git checkout 92b2a4a0c06ee9f6ab2b972ed7b202031ecac775
-popd
 
 # install dependencies
 rm -rf $HOME/miniconda/envs/electrificationplanner
-conda create -n electrificationplanner -f infrastructure-planning/packages.txt
+conda create -n electrificationplanner -f packages.txt
 conda install -n electrificationplanner -c conda-forge -c sel infrastructure-planning
