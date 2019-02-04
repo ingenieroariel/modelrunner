504521584f817ab5c9481447afc2ca6ec7f1ec9a
Your Name <you@example.com>
2019-02-04 10:08:53 -0500

    Updated electrificationplanner to specific versions

--- devops/setup_electrificationplanner.sh
+++ devops/setup_electrificationplanner.sh
@@ -10,11 +10,10 @@ echo "Setup electrificationplanner env"
 # electrificationplanner requires full source to run
 rm -rf $HOME/infrastructure-planning
 git clone https://github.com/piensa/infrastructure-planning
+# get to commit with pinned versions
+git checkout e96f93c69422e9ef4f47582cade269a15cfabaee
 
 # install dependencies
 rm -rf $HOME/miniconda/envs/electrificationplanner
-conda create -n electrificationplanner python=2.7
+conda create -n electrificationplanner -f infrastructure-planning/packages.txt
 conda install -n electrificationplanner -c conda-forge -c sel infrastructure-planning
-
-conda install -n electrificationplanner gdal=2.2
-conda install -n electrificationplanner "poppler<0.62"
